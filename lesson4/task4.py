65dafc4cb8249f0422192217ad3e1425c67ac0f9
dyhmzall <dyhmzall@gmail.com>
2021-01-30 22:35:11 +0300

    рефакторинг задания 4

--- lesson4/task4.py
+++ lesson4/task4.py
@@ -7,4 +7,7 @@
 """
 source = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
-print([el for el in source if source.count(el) == 1])
+result = [el for el in source if source.count(el) == 1]
+
+print(f"Исходный список: {source}")
+print(f"Итоговый список: {result}")
